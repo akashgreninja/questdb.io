68222a9b4c644534085741ff95b28ac8840889ed
Amy Wang <107401731+amyshwang@users.noreply.github.com>
2022-10-05 11:34:02 +0100

    Adding new page to sidebars

--- sidebars.js
+++ sidebars.js
@@ -81,6 +81,7 @@ module.exports = {
         "operations/health-monitoring",
         "operations/backup",
         "operations/updating-data",
+        "operations/advanced-capacity-planning"
       ],
     },
     {
